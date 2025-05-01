c873bc1958d11cb69e273337f8bcb20231f7aa04
Marc Mendoza <777marc@gmail.com>
2025-05-01 10:27:55 -0400

    adding table

--- src/services/hello.js
+++ src/services/hello.js
@@ -1,6 +1,8 @@
 exports.main = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify("Hello From Lambda"),
+    body: JSON.stringify(
+      `Hello From Lambda, I'm reading from ${process.env.TABLE_NAME}`
+    ),
   };
 };
