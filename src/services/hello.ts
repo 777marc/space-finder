34b8044a075812965b1241aaefb29d85e3c32806
Marc Mendoza <777marc@gmail.com>
2025-05-01 13:33:54 -0400

    adding sdk usage

--- src/services/hello.ts
+++ src/services/hello.ts
@@ -4,14 +4,21 @@ import {
   Context,
 } from "aws-lambda";
 import { v4 } from "uuid";
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+  const command = new ListBucketsCommand({});
+  const listBucketResults = (await s3Client.send(command)).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify(
       `Hello From Lambda, I'm reading from ${
         process.env.TABLE_NAME
-      } and id: ${v4()}`
+      } and id: ${v4()}
+      here are your buckets: ${JSON.stringify(listBucketResults)}`
     ),
   };
   console.log(event);
