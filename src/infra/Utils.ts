fec2fffbad3d7e1dfd6abbd5cc62285ac243a0e3
Marc Mendoza <777marc@gmail.com>
2025-05-07 12:38:09 -0400

    comment out groups - temp

--- src/infra/Utils.ts
+++ src/infra/Utils.ts
@@ -8,9 +8,10 @@ export function getSuffixFromStack(stack: Stack) {
 }
 
 export function hasAdminGroup(event: APIGatewayProxyEvent) {
-  const groups = event.requestContext.authorizer?.claims["cognito:groups"];
-  if (groups) {
-    return (groups as string).includes("admins");
-  }
-  return false;
+  // const groups = event.requestContext.authorizer?.claims["cognito:groups"];
+  // if (groups) {
+  //   return (groups as string).includes("admins");
+  // }
+  // return false;
+  return true;
 }
--- src/services/spaces/DeleteSpace.ts
+++ src/services/spaces/DeleteSpace.ts
@@ -6,12 +6,12 @@ export async function deleteSpace(
   event: APIGatewayProxyEvent,
   ddbClient: DynamoDBClient
 ): Promise<APIGatewayProxyResult> {
-  if (!hasAdminGroup(event)) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify("Not Authorized to delete."),
-    };
-  }
+  // if (!hasAdminGroup(event)) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify("Not Authorized to delete."),
+  //   };
+  // }
 
   if (event.queryStringParameters && "id" in event.queryStringParameters) {
     if ("id" in event.queryStringParameters) {
